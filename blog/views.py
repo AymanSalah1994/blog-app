8b0b8079a9727399e4de598fbe5d7fdfa204bb13
Ayman Salah <ayman.1551.salah@gmail.com>
2022-03-21 04:13:51 +0200

    Configured internal links

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
+from email import contentmanager
+from multiprocessing import context
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 from blog.models import Post
 
 
 class BlogListView(ListView):
-
     model = Post
     template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    # It does Not take pk But it is Expecting it !!
+    model = Post
+    template_name = 'post_detail.html'
+    # context_object_name = "bla"
