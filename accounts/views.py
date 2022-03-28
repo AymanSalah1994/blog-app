a5c942e9c0fb5d8426684663989cf531aaa9f8f8
Ayman Salah <ayman.1551.salah@gmail.com>
2022-03-28 10:31:57 +0200

    Added Some Useful Comments

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,10 @@ from django.urls import reverse_lazy
 
 
 class SignUpView(CreateView):
+    # Inherit From CreateView So That , it is a Creation Process 
     form_class = UserCreationForm
+    #  This is T send a "Form Context processor" to our template ,
+    # Form == Model & Vice Versa , If we Use a Pre-Build Form , this means There are Corresponding Model Fields  ; 
     success_url = reverse_lazy('login')
+    # ???
     template_name = 'registration/signup.html'
